572592421f8b0c5d388eeff4c8dac65792630d73
Filip Begić <filip.begic@gdi.net>
2020-02-17 10:06:22 +0100

    save commit

--- PlannerAngular/src/app/planner/planner.module.ts
+++ PlannerAngular/src/app/planner/planner.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DateAdapter, CalendarModule } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { CalendarModule } from 'angular-calendar';
 import { PlannerViewComponent } from './planner-view/planner-view.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
